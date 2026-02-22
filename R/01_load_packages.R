aedb97e37b153f610cccf989b4b9873e1f7368be
anisurbali <52972188+anisurbali@users.noreply.github.com>
2026-02-22 22:19:55 +0600

    1

--- R/01_load_packages.R
+++ R/01_load_packages.R
@@ -9,4 +9,12 @@ library(ggplot2)
 library(showtext)
 library(sysfonts)
 
+#----------------------------------------------
+#project data location
+# ----------------------------------------------
 
+if (here() == "C:/Users/bmani/GitHub/budget-r-imed") {
+  data <- "G:/My Drive/Finance Division/Tripartite Meeting/tripartite-r-project-imed/data"
+} else {
+  "Not available"
+}
